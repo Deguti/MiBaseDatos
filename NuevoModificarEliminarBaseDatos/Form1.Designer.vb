d8981f3987826fe42acb1cdadd1a585970cf0b27
Derick Gutierrez <degutierrez1992@gmail.com>
2021-10-27 16:56:24 -0600

    mibasedatos

---
+++ NuevoModificarEliminarBaseDatos/Form1.Designer.vb
@@ -0,0 +1,320 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GridView1 = New System.Windows.Forms.DataGridView()
+        Me.LabelInfo = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextCliente = New System.Windows.Forms.TextBox()
+        Me.TextTerminos = New System.Windows.Forms.TextBox()
+        Me.TextNombre = New System.Windows.Forms.TextBox()
+        Me.TextEmpresa = New System.Windows.Forms.TextBox()
+        Me.TextCalle = New System.Windows.Forms.TextBox()
+        Me.TextCiudad = New System.Windows.Forms.TextBox()
+        Me.TextProvincia = New System.Windows.Forms.TextBox()
+        Me.TextCodigo = New System.Windows.Forms.TextBox()
+        Me.TextTelefono = New System.Windows.Forms.TextBox()
+        Me.TextCorreo = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GridView1
+        '
+        Me.GridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GridView1.Location = New System.Drawing.Point(16, 12)
+        Me.GridView1.Name = "GridView1"
+        Me.GridView1.Size = New System.Drawing.Size(776, 188)
+        Me.GridView1.TabIndex = 3
+        '
+        'LabelInfo
+        '
+        Me.LabelInfo.AutoSize = True
+        Me.LabelInfo.Location = New System.Drawing.Point(21, 428)
+        Me.LabelInfo.Name = "LabelInfo"
+        Me.LabelInfo.Size = New System.Drawing.Size(35, 13)
+        Me.LabelInfo.TabIndex = 2
+        Me.LabelInfo.Text = "label1"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(24, 220)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(39, 13)
+        Me.label1.TabIndex = 4
+        Me.label1.Text = "Cliente"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 250)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Terminos"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 280)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Nombre"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(24, 310)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Empresa"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(24, 340)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(30, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Calle"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(434, 220)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Ciudad"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(434, 250)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(51, 13)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Provincia"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(434, 280)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(40, 13)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Codigo"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(434, 310)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(49, 13)
+        Me.Label9.TabIndex = 13
+        Me.Label9.Text = "Telefono"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(434, 340)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(38, 13)
+        Me.Label10.TabIndex = 14
+        Me.Label10.Text = "Correo"
+        '
+        'TextCliente
+        '
+        Me.TextCliente.Location = New System.Drawing.Point(78, 220)
+        Me.TextCliente.Name = "TextCliente"
+        Me.TextCliente.Size = New System.Drawing.Size(265, 20)
+        Me.TextCliente.TabIndex = 15
+        '
+        'TextTerminos
+        '
+        Me.TextTerminos.Location = New System.Drawing.Point(78, 250)
+        Me.TextTerminos.Name = "TextTerminos"
+        Me.TextTerminos.Size = New System.Drawing.Size(265, 20)
+        Me.TextTerminos.TabIndex = 16
+        '
+        'TextNombre
+        '
+        Me.TextNombre.Location = New System.Drawing.Point(80, 280)
+        Me.TextNombre.Name = "TextNombre"
+        Me.TextNombre.Size = New System.Drawing.Size(265, 20)
+        Me.TextNombre.TabIndex = 17
+        '
+        'TextEmpresa
+        '
+        Me.TextEmpresa.Location = New System.Drawing.Point(78, 310)
+        Me.TextEmpresa.Name = "TextEmpresa"
+        Me.TextEmpresa.Size = New System.Drawing.Size(265, 20)
+        Me.TextEmpresa.TabIndex = 18
+        '
+        'TextCalle
+        '
+        Me.TextCalle.Location = New System.Drawing.Point(78, 340)
+        Me.TextCalle.Name = "TextCalle"
+        Me.TextCalle.Size = New System.Drawing.Size(265, 20)
+        Me.TextCalle.TabIndex = 19
+        '
+        'TextCiudad
+        '
+        Me.TextCiudad.Location = New System.Drawing.Point(497, 220)
+        Me.TextCiudad.Name = "TextCiudad"
+        Me.TextCiudad.Size = New System.Drawing.Size(254, 20)
+        Me.TextCiudad.TabIndex = 20
+        '
+        'TextProvincia
+        '
+        Me.TextProvincia.Location = New System.Drawing.Point(497, 250)
+        Me.TextProvincia.Name = "TextProvincia"
+        Me.TextProvincia.Size = New System.Drawing.Size(254, 20)
+        Me.TextProvincia.TabIndex = 21
+        '
+        'TextCodigo
+        '
+        Me.TextCodigo.Location = New System.Drawing.Point(497, 280)
+        Me.TextCodigo.Name = "TextCodigo"
+        Me.TextCodigo.Size = New System.Drawing.Size(254, 20)
+        Me.TextCodigo.TabIndex = 22
+        '
+        'TextTelefono
+        '
+        Me.TextTelefono.Location = New System.Drawing.Point(497, 310)
+        Me.TextTelefono.Name = "TextTelefono"
+        Me.TextTelefono.Size = New System.Drawing.Size(254, 20)
+        Me.TextTelefono.TabIndex = 23
+        '
+        'TextCorreo
+        '
+        Me.TextCorreo.Location = New System.Drawing.Point(497, 340)
+        Me.TextCorreo.Name = "TextCorreo"
+        Me.TextCorreo.Size = New System.Drawing.Size(254, 20)
+        Me.TextCorreo.TabIndex = 24
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(78, 378)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 25
+        Me.Button1.Text = "Nuevo"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(174, 378)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 26
+        Me.Button2.Text = "Modificar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(268, 378)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 27
+        Me.Button3.Text = "Eliminar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextCorreo)
+        Me.Controls.Add(Me.TextTelefono)
+        Me.Controls.Add(Me.TextCodigo)
+        Me.Controls.Add(Me.TextProvincia)
+        Me.Controls.Add(Me.TextCiudad)
+        Me.Controls.Add(Me.TextCalle)
+        Me.Controls.Add(Me.TextEmpresa)
+        Me.Controls.Add(Me.TextNombre)
+        Me.Controls.Add(Me.TextTerminos)
+        Me.Controls.Add(Me.TextCliente)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.GridView1)
+        Me.Controls.Add(Me.LabelInfo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GridView1 As DataGridView
+    Friend WithEvents LabelInfo As Label
+    Friend WithEvents label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextCliente As TextBox
+    Friend WithEvents TextTerminos As TextBox
+    Friend WithEvents TextNombre As TextBox
+    Friend WithEvents TextEmpresa As TextBox
+    Friend WithEvents TextCalle As TextBox
+    Friend WithEvents TextCiudad As TextBox
+    Friend WithEvents TextProvincia As TextBox
+    Friend WithEvents TextCodigo As TextBox
+    Friend WithEvents TextTelefono As TextBox
+    Friend WithEvents TextCorreo As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ NuevoModificarEliminarBaseDatos/Form1.vb
@@ -0,0 +1,62 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Public stringConeccion As String = “Data Source = DESKTOP-2ROLQTO; Initial Catalog = Facturacion xx; Integrated Security = True”
+    Dim stringSelect As String = "SELECT * FROM Clientes"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call CargarGrid()
+
+    End Sub
+
+    Private Sub CargarGrid()
+
+        Dim da As SqlDataAdapter
+        Dim dt As New DataTable
+        Try
+            da = New SqlDataAdapter(stringSelect, stringConeccion)
+            da.Fill(dt)
+            Me.GridView1.DataSource = dt
+            'LabelInfo.Text = String.Format("Total datos en la tabla: {0}", dt.Rows.Count)
+            'String.Format("Total datos en la tabla: {0}", dt.Rows.Count)el 0 se sustituye por el valor de la par o que le asignes
+        Catch ex As Exception
+
+            LabelInfo.Text = "Error: " & ex.Message
+        End Try
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim NombreTabla As String = "clientes"
+        Dim InsertSql As String
+        InsertSql = "INSERT INTO " & NombreTabla & " (IdCliente, TerminosFactura, NombreCliente, NombreEmpresaCliente, "
+        InsertSql = InsertSql & " CalleCliente, Ciudad, Provincia, CodigoPostal, TelefonoCliente, DireccionCorreoElectronico ) " & "VALUES "
+        InsertSql = InsertSql & "(@IdCliente, @TerminosFactura, @NombreCliente, @NombreEmpresaCliente, "
+        InsertSql = InsertSql & " @CalleCliente, @Ciudad, @Provincia, @CodigoPostal, @TelefonoCliente, @DireccionCorreoElectronico )"
+
+        Using con As New SqlConnection(stringConeccion)
+
+            Dim cmd As New SqlCommand(insertsql, con)
+            cmd.Parameters.AddWithValue("@idCliente", Val(TextCliente.Text))
+            cmd.Parameters.AddWithValue("@TerminosFactura", TextTerminos.Text)
+            cmd.Parameters.AddWithValue("@NombreCliente", TextNombre.Text)
+            cmd.Parameters.AddWithValue("@NombreEmpresaCliente", TextEmpresa.Text)
+            cmd.Parameters.AddWithValue("@CalleCliente", TextCalle.Text)
+            cmd.Parameters.AddWithValue("@Ciudad", TextCiudad.Text)
+            cmd.Parameters.AddWithValue("@Provincia", TextProvincia.Text)
+            cmd.Parameters.AddWithValue("@CodigoPostal", TextCodigo.Text)
+            cmd.Parameters.AddWithValue("@TelefonoCliente", TextTelefono.Text)
+            cmd.Parameters.AddWithValue("@DireccionCorreoElectronico", TextCorreo.Text)
+
+            con.Open()
+            Dim t As Integer = CInt(cmd.ExecuteScalar())
+
+            con.Close()
+
+        End Using
+
+        Call CargarGrid()
+
+    End Sub
+End Class
+
+
